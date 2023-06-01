588b92b8dc2f2b0170129e31afe050f78ba5a1c0
amirhoseinadlfar <amirhoseinadlfar@gmail.com>
2023-06-01 12:23:03 +0330

    init 6

--- Program.cs
+++ Program.cs
@@ -9,8 +9,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
 }
 
 app.UseHttpsRedirection();
@@ -22,6 +24,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-//app.MapFallbackToFile("index.html");
+app.MapFallbackToFile("index.html");
 
 app.Run();
